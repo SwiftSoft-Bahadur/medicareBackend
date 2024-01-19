8e7f951ab372334e2bf252f07cab3e151f6ae478
Bahadur <bk209203@gmail.com>
2024-01-19 17:46:34 +0530

    package.json script updated code

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
     jwtSecret: process.env.jwtSecret,
-    mongoURL: process.env.mongoURL,
+    mongoURL: process.env.mongoURL || "mongodb://localhost/medicare",
     redisHOST: process.env.redisHOST,
     redisPORT: process.env.redisPORT,
     redisPassword: process.env.redisPassword,
-    port: process.env.PORT
+    port: process.env.PORT || 3000
 };

3b12f6f067bba4a61bf7b4f0be0671f12e55fc6c
Bahadur <bk209203@gmail.com>
2024-01-23 22:03:35 +0530

    package.json script updated code

--- config/redis.js
+++ config/redis.js
@@ -1,61 +1,47 @@
-// const redis = require('redis');
-// const config = require('../config/index');
-
-// let client;
-
-// function promisifyHget(client, key) {
-//     return new Promise((resolve, reject) => {
-//         client.hget(key, (err, result) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(result);
-//             }
-//         });
-//     });
-// }
-
-// module.exports = {
-//     getClient: () => {
-//         if (!client) {
-//             console.log("Reinitializing REDIS!!!!!!!!!!!!!!");
-//             const redisConfig = {
-//                 host: config.redisHOST,
-//                 port: config.redisPORT
-//             };
-
-//             if (process.env.NODE_ENV === "production") {
-//                 redisConfig.password = config.redisPassword;
-//             }
-
-//             client = redis.createClient(redisConfig);
-//             console.log(client);
-//             client.hgetAsync = (key) => promisifyHget(client, key);
-//         }
-//         return client;
-//     }
-// };
-
-
-const util = require('util');
 const redis = require('redis');
 const config = require('../config/index');
 
 let client;
 
+function promisifyHget(client, key) {
+    return new Promise((resolve, reject) => {
+        client.hget(key, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
 module.exports = {
     getClient: () => {
         if (!client) {
-            console.log("reinitializing REDIS!!!!!!!!!!!!!!");
-            redisConfig = {
+            console.log("Reinitializing REDIS!!!!!!!!!!!!!!");
+            const redisConfig = {
                 host: config.redisHOST,
                 port: config.redisPORT
             };
+
             if (process.env.NODE_ENV === "production") {
                 redisConfig.password = config.redisPassword;
             }
+
             client = redis.createClient(redisConfig);
-            client.hget = util.promisify(client.hget);
+
+            // Check if hget method exists before manually promisifying
+            if (client.hget) {
+                client.hgetAsync = (key) => promisifyHget(client, key);
+            }
+
+            // Handle MaxListenersExceededWarning by increasing the limit
+            client.setMaxListeners(15); // Set an appropriate number based on your application
+
+            // Add error event listener to log Redis connection errors
+            client.on('error', (err) => {
+                console.error('Redis connection error:', err);
+            });
         }
         return client;
     }
